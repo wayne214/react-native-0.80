6a935d62a2da443ce1812394784ac3e9b0f9c6d8
wangjunwei <1032928762@qq.com>
2025-12-30 15:13:38 +0800

    add react-native-error-boundary

--- src/zud_store/Counter.tsx
+++ src/zud_store/Counter.tsx
@@ -2,6 +2,9 @@ import '../../global.css'
 import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { z } from 'zod';
 import { useCounterStore } from './useCounterStore.ts';
+import ErrorBoundary from "react-native-error-boundary";
+import ComponentWithError from "../ComponentWithError.tsx";
+import {useState} from "react";
 
 const UserSchema = z.object({
   id: z.number(),
@@ -11,6 +14,11 @@ const UserSchema = z.object({
 
 function Counter() {
   const { count, inc, dec } = useCounterStore()
+  const [showErrorBoundary, setShowErrorBoundary] = useState(false);
+
+  const checkError = () => {
+    setShowErrorBoundary(true)
+   }
 
   const checkData = () => {
     const result = UserSchema.safeParse({
@@ -18,31 +26,48 @@ function Counter() {
       name: '张三',
       age: 18,
     });
+
     console.log("---数据===",result)
     if (result.success) {
       console.log("---数据===",result.data)
     }
   };
 
-  return (
-    <View style={styles.container}>
-      <View className="w-300 h-200 items-center justify-center bg-red-500">
-        <Text className="text-xl font-bold text-blue-500">
-          Welcome to Nativewind!
-        </Text>
+  const ErrorFallback = (props) => (
+      <View style={styles.container}>
+        <Text style={styles.title}>Something happened!</Text>
+        <Text style={styles.text}>{props.error.toString()}</Text>
       </View>
+  )
 
-      <Text style={styles.text}>{count}</Text>
-      <Pressable onPress={inc} style={styles.button}>
-        <Text>加1</Text>
-      </Pressable>
-      <Pressable onPress={dec} style={styles.button}>
-        <Text>减-</Text>
-      </Pressable>
-      <Pressable onPress={checkData} style={styles.button}>
-        <Text>校验数据</Text>
-      </Pressable>
-    </View>
+  return (
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <View style={styles.container}>
+          <View className="w-300 h-200 items-center justify-center bg-red-500">
+            <Text className="text-xl font-bold text-blue-500">
+              Welcome to Nativewind!
+            </Text>
+          </View>
+
+          <Text style={styles.text}>{count}</Text>
+          <Pressable onPress={inc} style={styles.button}>
+            <Text>加1</Text>
+          </Pressable>
+          <Pressable onPress={dec} style={styles.button}>
+            <Text>减-</Text>
+          </Pressable>
+          <Pressable onPress={checkData} style={styles.button}>
+            <Text>校验数据</Text>
+          </Pressable>
+          <Pressable onPress={checkError} style={styles.button}>
+            <Text>错误边界</Text>
+          </Pressable>
+          {
+            showErrorBoundary && <ComponentWithError />
+          }
+        </View>
+      </ErrorBoundary>
+
   )
 }
 export const styles = StyleSheet.create({
@@ -63,10 +88,15 @@ export const styles = StyleSheet.create({
     width: 100,
     height: 50
   },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  icon: {
+    fontSize: 48
+  },
 });
 
 export default Counter;
-function alert(arg0: string) {
-    throw new Error('Function not implemented.');
-}
 
