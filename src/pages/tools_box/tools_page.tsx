a1d2f8052163c4ae008d61fab26bcf3d0779f19e
wangjunwei <1032928762@qq.com>
2026-01-06 09:03:08 +0800

    add(toolbox): Integrate react-native-calendars and update date picker
    - Add `react-native-calendars` to dependencies
    - Update `ToolsPage` to include a calendar component alongside the date picker

--- src/pages/tools_box/tools_page.tsx
+++ src/pages/tools_box/tools_page.tsx
@@ -1,21 +1,36 @@
 import React, { useState } from "react"
+import { View } from "react-native";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import DateTimePicker, { DateType, useDefaultStyles } from 'react-native-ui-datepicker';
 
 const ToolsPage = () => {
   const defaultStyles = useDefaultStyles();
   const [selected, setSelected] = useState<DateType>();
+  const [selectedDate, setSelectedDate] = useState('');
   return (
-    <DateTimePicker
-      mode="single"
-      date={selected}
-      onChange={({ date }) =>  setSelected(date)}
-      styles={{
-        ...defaultStyles,
-        today: { borderColor: 'blue', borderWidth: 1 }, // Add a border to today's date
-        selected: { backgroundColor: 'blue' }, // Highlight the selected day
-        selected_label: { color: 'white' }, // Highlight the selected day label
-      }}
-    />
+    <View>
+      <DateTimePicker
+        mode="single"
+        date={selected}
+        onChange={({ date }) =>  setSelected(date)}
+        styles={{
+          ...defaultStyles,
+          today: { borderColor: 'blue', borderWidth: 1 }, // Add a border to today's date
+          selected: { backgroundColor: 'blue' }, // Highlight the selected day
+          selected_label: { color: 'white' }, // Highlight the selected day label
+        }}
+      />
+
+      <Calendar
+        onDayPress={day => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selectedDate]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+        }}
+      />
+    </View>
+
   );
 }
 
