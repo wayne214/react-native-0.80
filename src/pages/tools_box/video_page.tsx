87a88e97553628f93dc1cb621ca2cb679bf0a948
wangjunwei <1032928762@qq.com>
2026-01-06 16:41:10 +0800

    update(video_page): Change video source URL and add test URLs comments

--- src/pages/tools_box/video_page.tsx
+++ src/pages/tools_box/video_page.tsx
@@ -3,8 +3,23 @@ import { StyleSheet, View } from "react-native";
 import { useVideoPlayer, VideoView } from 'react-native-video';
 
 const VideoPage = () => {
+
+  const HLS = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+  const MP4 =
+    'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_30MB.mp4';
+  /**
+   * 一些测试地址;
+   * https://www.w3schools.com/html/mov_bbb.mp4
+   * https://www.w3schools.com/html/movie.mp4
+   * https://media.w3.org/2010/05/sintel/trailer.mp4
+   * https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
+   *
+   * const HLS = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+   *   const MP4 =
+   *     'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_30MB.mp4';
+   * */
   const player = useVideoPlayer(
-    'https://www.w3schools.com/html/mov_bbb.mp4',
+    'https://www.w3schools.com/html/movie.mp4',
     (_player) => {
       _player.play();
     }
